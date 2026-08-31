454c67ef593a9ec51036043e2c796048f3d07393
gmclean3107 <gman3107@outlook.com>
2026-08-31 22:35:42 +0100

    Updated logic

--- repl.go
+++ repl.go
@@ -4,7 +4,7 @@ import "strings"
 
 func cleanInput(input string) []string {
 	splitString := []string{}
-
+	input = strings.ToLower(input)
 	for _, word := range strings.Split(input, " ") {
 		if word != "" {
 			splitString = append(splitString, word)

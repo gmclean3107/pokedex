b282ca945841a1c0bd450ef3825c0f345ed50148
gmclean3107 <gman3107@outlook.com>
2026-09-04 23:07:02 +0100

    Explore functionality implemented

--- command_explore.go
+++ command_explore.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func commandExplore(cfg *config) error {
+
+	if len(cfg.arguments) < 1 {
+		return errors.New("you must provide a location name")
+	}
+
+	locationResp, err := cfg.pokeapiClient.ExploreLocation(cfg.arguments[0])
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Exploring %s...\n", cfg.arguments[0])
+	fmt.Println("Found Pokemon: ")
+
+	for _, encounter := range locationResp.Pokemon_encounters {
+		fmt.Println(encounter.Pokemon.Name)
+	}
+
 	return nil
 }
--- internal/pokeapi/explore_location.go
+++ internal/pokeapi/explore_location.go
@@ -1,7 +1,45 @@
 package pokeapi
 
-//Explore Location
-func (c *Client) ExploreLocation(location string) []string {
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Explore Location
+func (c *Client) ExploreLocation(location string) (Location, error) {
 	url := baseURL + "/location-area/" + location
-	return []string{}
+
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := Location{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return Location{}, err
+		}
+		return locationResp, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	defer res.Body.Close()
+
+	locationResp := Location{}
+
+	err = decoder.Decode(&locationResp)
+
+	if err != nil {
+		return Location{}, err
+	}
+
+	return locationResp, nil
 }
